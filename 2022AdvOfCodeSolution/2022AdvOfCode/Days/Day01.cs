5377e57fc3e475b805212f19e0df0557b5033bbc
anna4615 <anna4615@hotmail.com>
2022-12-11 12:03:05 +0100

    add 2022 project

---
+++ 2022AdvOfCodeSolution/2022AdvOfCode/Days/Day01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdvOfCode.Days
+{
+    public class Day01 : AdventDay
+    {
+        public int GetAnswer()
+        {
+            return 24000;
+        }
+    }
+}
---
+++ 2022AdvOfCodeSolution/2022AdvTest/Tests/Day01Test.cs
@@ -0,0 +1,30 @@
+﻿using _2022AdvOfCode.Days;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace _2022AdvTest.Tests
+{
+    [TestClass]
+    public class Day01Test
+    {
+        public Day01 Day { get; set; }
+
+        [TestInitialize]
+        public void Init()
+        {
+            Day = new Day01();
+        }
+
+        [TestMethod]
+        public void TestGetAnswer()
+        {
+            int calories = Day.GetAnswer();
+            Assert.AreEqual(24000, calories);
+        }
+    }
+}
